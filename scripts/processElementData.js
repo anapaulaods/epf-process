36c95273f72b3469ccd613a630d335b34b38d238
anapaulaods <ana.paula1997@hotmail.com>
2021-04-21 22:32:08 -0300

    Atividade para a disciplina de processo de software

---
+++ scripts/processElementData.js
@@ -0,0 +1,51 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_1h5-AaLyEeuyz71XJG3B2Q,_VIo-YaL3Eeuyz71XJG3B2QActivityDetail"]="./../../anapaula/deliveryprocesses/resources/Validação_642579F2_a042334_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1h5-AaLyEeuyz71XJG3B2Q,_9NudYaL2Eeuyz71XJG3B2QActivityDetail"]="./../../anapaula/deliveryprocesses/resources/Desenvolvimento_A986BDE4_4f656726_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1h5-AaLyEeuyz71XJG3B2Q,_YJHs0aL3Eeuyz71XJG3B2QActivityDetail"]="./../../anapaula/deliveryprocesses/resources/Implantação_7BE17AA6_21c023e8_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1h5-AaLyEeuyz71XJG3B2Q,_C7HeAaL3Eeuyz71XJG3B2QActivityDetail"]="./../../anapaula/deliveryprocesses/resources/Demandas_CB67E3EA_71468d2c_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1h5-AaLyEeuyz71XJG3B2Q,_NW-_8aL3Eeuyz71XJG3B2QActivityDetail"]="./../../anapaula/deliveryprocesses/resources/Teste_FD14BF3D_a2f3687f_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["_H6ch-KL7Eeuyz71XJG3B2Q"] = ["Implantação","anapaula/workproducts/Implantação_1696F695.html"];
+contentPage.processPage.elementUrls["_t4TZQ6L7Eeuyz71XJG3B2Q"] = ["Implantação","anapaula/workproducts/Implantação_1696F695.html"];
+contentPage.processPage.elementUrls["_H6ch8qL7Eeuyz71XJG3B2Q"] = ["Codigo Corrigido","anapaula/workproducts/Codigo Corrigido_ED13BE16.html"];
+contentPage.processPage.elementUrls["_mZOYkaL7Eeuyz71XJG3B2Q"] = ["Implantação","anapaula/workproducts/Implantação_1696F695.html"];
+contentPage.processPage.elementUrls["_ytF98KL7Eeuyz71XJG3B2Q"] = ["Corrigir bugs","anapaula/tasks/Corrigir bugs_886C2323.html"];
+contentPage.processPage.elementUrls["_mZKuMKL7Eeuyz71XJG3B2Q"] = ["Desenvolvedor","anapaula/roles/Desenvolvedor_138A9F73.html"];
+contentPage.processPage.elementUrls["_H6ch9aL7Eeuyz71XJG3B2Q"] = ["Codigo Testado","anapaula/workproducts/Codigo Testado_5D40AF00.html"];
+contentPage.processPage.elementUrls["_H45p0qL7Eeuyz71XJG3B2Q"] = ["Codigo","anapaula/workproducts/Codigo_5749A17E.html"];
+contentPage.processPage.elementUrls["_qT9IyKL7Eeuyz71XJG3B2Q"] = ["Implantação","anapaula/workproducts/Implantação_1696F695.html"];
+contentPage.processPage.elementUrls["_H45p0KL7Eeuyz71XJG3B2Q"] = ["Desenvolvedor","anapaula/roles/Desenvolvedor_138A9F73.html"];
+contentPage.processPage.elementUrls["_t4Zf5KL7Eeuyz71XJG3B2Q"] = ["Codigo Testado","anapaula/workproducts/Codigo Testado_5D40AF00.html"];
+contentPage.processPage.elementUrls["_t4Zf4aL7Eeuyz71XJG3B2Q"] = ["Codigo Corrigido","anapaula/workproducts/Codigo Corrigido_ED13BE16.html"];
+contentPage.processPage.elementUrls["_sgyzwKL7Eeuyz71XJG3B2Q"] = ["Validar o desenvolvimento","anapaula/tasks/Validar o desenvolvimento_943D643.html"];
+contentPage.processPage.elementUrls["_qT9IxaL7Eeuyz71XJG3B2Q"] = ["Codigo Corrigido","anapaula/workproducts/Codigo Corrigido_ED13BE16.html"];
+contentPage.processPage.elementUrls["_VIo-YaL3Eeuyz71XJG3B2Q"] = ["Validação","anapaula/deliveryprocesses/Validação_642579F2.html"];
+contentPage.processPage.elementUrls["_t4TZQKL7Eeuyz71XJG3B2Q"] = ["Fazer deploy","anapaula/tasks/Fazer deploy_A0BA1D19.html"];
+contentPage.processPage.elementUrls["_1h5-AaLyEeuyz71XJG3B2Q"] = ["AnaPaula_Estagio","anapaula/deliveryprocesses/AnaPaula_Estagio_22FE84EE.html"];
+contentPage.processPage.elementUrls["_mZNKcaL7Eeuyz71XJG3B2Q"] = ["Codigo","anapaula/workproducts/Codigo_5749A17E.html"];
+contentPage.processPage.elementUrls["_mZNxgaL7Eeuyz71XJG3B2Q"] = ["Codigo Corrigido","anapaula/workproducts/Codigo Corrigido_ED13BE16.html"];
+contentPage.processPage.elementUrls["_t4TZQaL7Eeuyz71XJG3B2Q"] = ["Desenvolvedor","anapaula/roles/Desenvolvedor_138A9F73.html"];
+contentPage.processPage.elementUrls["_YJHs0aL3Eeuyz71XJG3B2Q"] = ["Implantação","anapaula/deliveryprocesses/Implantação_7BE17AA6.html"];
+contentPage.processPage.elementUrls["_NW-_8aL3Eeuyz71XJG3B2Q"] = ["Teste","anapaula/deliveryprocesses/Teste_FD14BF3D.html"];
+contentPage.processPage.elementUrls["_9NudYaL2Eeuyz71XJG3B2Q"] = ["Desenvolvimento","anapaula/deliveryprocesses/Desenvolvimento_A986BDE4.html"];
+contentPage.processPage.elementUrls["_mZNxhKL7Eeuyz71XJG3B2Q"] = ["Codigo Testado","anapaula/workproducts/Codigo Testado_5D40AF00.html"];
+contentPage.processPage.elementUrls["_qT9IwqL7Eeuyz71XJG3B2Q"] = ["Codigo","anapaula/workproducts/Codigo_5749A17E.html"];
+contentPage.processPage.elementUrls["_mZKHIKL7Eeuyz71XJG3B2Q"] = ["Prestar suporte ao portal","anapaula/tasks/Prestar suporte ao portal_48A86BF1.html"];
+contentPage.processPage.elementUrls["_t4Y40qL7Eeuyz71XJG3B2Q"] = ["Codigo","anapaula/workproducts/Codigo_5749A17E.html"];
+contentPage.processPage.elementUrls["_qTz-0aL7Eeuyz71XJG3B2Q"] = ["Desenvolvedor","anapaula/roles/Desenvolvedor_138A9F73.html"];
+contentPage.processPage.elementUrls["_C7HeAaL3Eeuyz71XJG3B2Q"] = ["Demandas","anapaula/deliveryprocesses/Demandas_CB67E3EA.html"];
+contentPage.processPage.elementUrls["_qTz-0KL7Eeuyz71XJG3B2Q"] = ["Testar em servidores internos","anapaula/tasks/Testar em servidores internos_56033B7B.html"];
+contentPage.processPage.elementUrls["_H45CwKL7Eeuyz71XJG3B2Q"] = ["Desenvolver novas páginas","anapaula/tasks/Desenvolver novas páginas_FF5694E7.html"];
+contentPage.processPage.elementUrls["_sgza0KL7Eeuyz71XJG3B2Q"] = ["Supervisor","anapaula/roles/Supervisor_529843EB.html"];
+contentPage.processPage.elementUrls["_qT0l4KL7Eeuyz71XJG3B2Q"] = ["Codigo Testado","anapaula/workproducts/Codigo Testado_5D40AF00.html"];
